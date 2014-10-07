3ac72f83af8f6f78089b85f47f7a36a234a3251c
fractalram <ram@fractalio.com>
2014-10-07 12:47:57 +0530

    IntegralStor ISCSI implementation based on ISTGT

---
+++ src/istgt_md5.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2008-2010 Daisuke Aoyama <aoyama@peach.ne.jp>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <inttypes.h>
+#include <stdint.h>
+
+#include <stddef.h>
+
+#include "istgt.h"
+#include "md5.h"
+#include "istgt_md5.h"
+
+int
+istgt_md5init(ISTGT_MD5CTX *md5ctx)
+{
+	if (md5ctx == NULL)
+		return -1;
+	md5_init(&md5ctx->state);
+	return 0;
+}
+
+int
+istgt_md5final(void *md5, ISTGT_MD5CTX *md5ctx)
+{
+	if (md5ctx == NULL || md5 == NULL)
+		return -1;
+	md5_finish(&md5ctx->state, md5);
+	return 0;
+}
+
+int
+istgt_md5update(ISTGT_MD5CTX *md5ctx, const void *data, size_t len)
+{
+	if (md5ctx == NULL)
+		return -1;
+	if (data == NULL || len <= 0)
+		return 0;
+	md5_append(&md5ctx->state, (const md5_byte_t *)data, (int)len);
+	return 0;
+}
